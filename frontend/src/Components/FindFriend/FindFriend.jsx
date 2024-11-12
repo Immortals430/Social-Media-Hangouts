7b109319eb28e285e2ac2a77f1ccbd8abfe10909
Immortals430 <vishalkumar619430@gmail.com>
2024-11-12 08:23:13 +0530

    updated

--- frontend/src/Components/FindFriend/FindFriend.jsx
+++ frontend/src/Components/FindFriend/FindFriend.jsx
@@ -7,7 +7,6 @@ import {
 } from "../../redux/reducers/friend_reducer";
 import { Link } from "react-router-dom";
 
-
 export default function FindFriend() {
   const { friendReqsRec, friendSuggestions } = useSelector(friendsSelector);
   const dispatch = useDispatch();
@@ -18,8 +17,8 @@ export default function FindFriend() {
   }
 
   function calltoggleFrndReq(e, friendId) {
-    e.preventDefault()
-    dispatch(toggleFrndReq(friendId))
+    e.preventDefault();
+    dispatch(toggleFrndReq(friendId));
   }
   return (
     <main className="friends">
@@ -38,31 +37,35 @@ export default function FindFriend() {
           <div className="friend-req-head">
             <h2>Friend Request</h2>
           </div>
-          {friendReqsRec.map(({ user }) => (
-            <Link to={`/profile/${user._id}`} key={user._id}>
-              <div className="user-card">
-                <div
+
+          <div className="grid">
+            {friendReqsRec.map(({ user }) => (
+              <Link to={`/profile/${user._id}`} key={user._id}>
+                <div className="user-card">
+                  <img src={user.avatarUrl} alt="" className="user-image" />
+                  {/* <div
                   className="user-image"
                   style={{ backgroundImage: `url(${user.avatarUrl})`, height: "208px" }}
-                ></div>
-                <div className="details">
-                  <h2>{user.username}</h2>
-                  <div
-                    className="accept-btn"
-                    onClick={(e) => callRespondReq(e, user._id, "accept")}
-                  >
-                    Accept
-                  </div>
-                  <div
-                    className="reject-btn"
-                    onClick={(e) => callRespondReq(e, user._id, "reject")}
-                  >
-                    Reject
+                ></div> */}
+                  <div className="details">
+                    <h3>{user.username}</h3>
+                    <div
+                      className="accept-btn"
+                      onClick={(e) => callRespondReq(e, user._id, "accept")}
+                    >
+                      Accept
+                    </div>
+                    <div
+                      className="reject-btn"
+                      onClick={(e) => callRespondReq(e, user._id, "reject")}
+                    >
+                      Reject
+                    </div>
                   </div>
                 </div>
-              </div>
-            </Link>
-          ))}
+              </Link>
+            ))}
+          </div>
         </section>
       ) : null}
 
@@ -71,34 +74,38 @@ export default function FindFriend() {
           <div className="friend-sugg-head">
             <h2>People you may know</h2>
           </div>
-          {friendSuggestions.map((user) => (
-            <Link to={`/profile/${user._id}`} key={user._id}>
-              <div className="user-card">
-                <div
+
+          <div className="grid">
+            {friendSuggestions.map((user) => (
+              <Link to={`/profile/${user._id}`} key={user._id}>
+                <div className="user-card">
+                  <img src={user.avatarUrl} alt="" className="user-image" />
+                  {/* <div
                   className="user-image"
                   style={{ backgroundImage: `url(${user.avatarUrl})` }}
-                ></div>
-                <div className="details">
-                  <h2>{user.username}</h2>
-                  {!user.reqSent ? (
-                    <div
-                      className="send-req-btn"
-                      onClick={(e) => calltoggleFrndReq(e, user._id)}
-                    >
-                      Send Request
-                    </div>
-                  ) : (
-                    <div
-                      className="reject-btn"
-                      onClick={(e) => calltoggleFrndReq(e, user._id)}
-                    >
-                      Cancel Request
-                    </div>
-                  )}
+                ></div> */}
+                  <div className="details">
+                    <h3>{user.username}</h3>
+                    {!user.reqSent ? (
+                      <div
+                        className="send-req-btn"
+                        onClick={(e) => calltoggleFrndReq(e, user._id)}
+                      >
+                        Send Request
+                      </div>
+                    ) : (
+                      <div
+                        className="reject-btn"
+                        onClick={(e) => calltoggleFrndReq(e, user._id)}
+                      >
+                        Cancel Request
+                      </div>
+                    )}
+                  </div>
                 </div>
-              </div>
-            </Link>
-          ))}
+              </Link>
+            ))}
+          </div>
         </section>
       ) : null}
     </main>
